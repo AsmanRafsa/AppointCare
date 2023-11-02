5a7a6d6e46bb1f28da96decb372a5eb6edafe10e
Rafsa Asman <aspharnamsat@gmail.com>
2023-11-02 19:12:21 +0300

    finished search and filtering

--- src/components/Booking.jsx
+++ src/components/Booking.jsx
@@ -10,7 +10,7 @@ import axios from "axios";
 function Booking() {
   // const { book, setBook, hospitals, setHospitals } = useContext(StateContext);
   const [searchTerm, setSearchTerm] = useState("");
-  const [filterOption, setFilterOption] = useState('all');
+  const [filterOption, setFilterOption] = useState("all");
   const [items, setItems] = useState([]);
   const { isLogIn, setIsLogIn } = useContext(StateContext);
   const [hospitals, setHospitals] = useState([]);
@@ -30,6 +30,9 @@ function Booking() {
         setError(err);
         setLoading(false);
       });
+    console.log(
+      hospitals.filter((hospital) => hospital.hospital_Location.includes("Na"))
+    );
   }, []);
 
   if (loading) {
@@ -47,7 +50,7 @@ function Booking() {
       <div className="border-2 flex border-blue-400 rounded-full p-6 container my-8 mx-auto">
         <BiSearch size={40} />
         <input
-          type="text"
+          type="search"
           placeholder="Search"
           className="border-none outline-none w-[100%]"
           value={searchTerm}
@@ -57,32 +60,40 @@ function Booking() {
 
       <div className="flex justify-center gap-6 items-center flex-wrap my-[2em]">
         {hospitals.map((hospital) => {
-          return (
-            <div className=" flex bg-blue-100  flex-col  w-[30%] items-center rounded-lg">
-              {console.log(searchTerm)}
-              <img
-                src={`${imageUrl}${hospital.hospital_Image}`}
-                alt=""
-                className="h-[30vh] w-[30vh] object-contain flex justify-center "
-                width={500}
-                height={500}
-              />
-              <div className="flex flex-col items-center">
-                <div className="flex justify-between text-blue-400">
-                  <p className="text-3xl"> {hospital.related_data.name}</p>
-                </div>
+          if (
+            hospital.related_data.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            hospital.hospital_Location.toLowerCase().includes(searchTerm.toLowerCase())
+          ) {
+            return (
+              <div
+                key={hospital.id}
+                className=" flex bg-blue-100  flex-col  w-[30%] items-center rounded-lg"
+              >
+                {console.log(searchTerm)}
+                <img
+                  src={`${imageUrl}${hospital.hospital_Image}`}
+                  alt=""
+                  className="h-[30vh] w-[30vh] object-contain flex justify-center "
+                  width={500}
+                  height={500}
+                />
+                <div className="flex flex-col items-center">
+                  <div className="flex justify-between text-blue-400">
+                    <p className="text-3xl"> {hospital.related_data.name}</p>
+                  </div>
 
-                <p className="text-2xl text-center w-[80%]">
-                  {hospital.hospital_Location}
-                </p>
-                <Link to={`/hospital/${hospital.id}`}>
-                  <button className="bg-[#3ba0f3] rounded-full py-3 my-4 text-white px-10">
-                    Book Now
-                  </button>
-                </Link>
+                  <p className="text-2xl text-center w-[80%]">
+                    {hospital.hospital_Location}
+                  </p>
+                  <Link to={`/hospital/${hospital.id}`}>
+                    <button className="bg-[#3ba0f3] rounded-full py-3 my-4 text-white px-10">
+                      Book Now
+                    </button>
+                  </Link>
+                </div>
               </div>
-            </div>
-          );
+            );
+          }
         })}
       </div>
       <Footer />
